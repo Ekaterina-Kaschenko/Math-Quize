db749b5406d433326b8bfc2a45c5cc13dc30c6d9
Ekaterina Kashchenko <katikate123@gmail.com>
2016-07-20 21:32:32 +0300

    doing task

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,10 @@
 window.onload = (function() {
   'use strict';
-  var bar = document.getElementsByClassName('progress-bar')[0];
-  var barProgress = document.getElementsByClassName('progress-bar-condition')[0];
-  barProgress.style.width = 0;
-  var count = 0; //progressbar count
+  // var count;
 
   function Quize() {
     this.result = null;
+    this.mistakenResult = null;
     this.startGameBlock = document.getElementsByClassName('start-game')[0];
     this.playGameBlock = document.getElementsByClassName('play-game')[0];
     this.endGameBlock = document.getElementsByClassName('end-game')[0];
@@ -14,6 +12,9 @@ window.onload = (function() {
     this.yesButton = document.getElementsByClassName('yes')[0];
     this.noButton = document.getElementsByClassName('no')[0];
     this.score = document.getElementsByClassName('score')[0];
+    this.bar = document.getElementsByClassName('progress-bar')[0];
+    this.barProgress = document.getElementsByClassName('progress-bar-condition')[0];
+    
   }
 
   var quize = new Quize();
@@ -25,17 +26,18 @@ window.onload = (function() {
 
     startButton.addEventListener("click", function() {
       getTask();
-      updateProgressBar();
+      quize.score = 0;
 	  });
   }
 
 	function getTask() {
     quize.startGameBlock.style.display = 'none';
     quize.playGameBlock.style.display = 'block';
-    progress ();
+    progressBar ();
     var a = getRandomInt(1,20);
     var b = getRandomInt(1,20);
-    var result = a + b;
+    quize.result = a + b;
+    quize.mistakenResult  = getRandomInt(1,20);
 
     var aValue = document.querySelector(".a-value");
     aValue.textContent = a;
@@ -44,75 +46,86 @@ window.onload = (function() {
     bValue.textContent = b;
 
     var answer = document.querySelector('.answer');
-    answer.textContent = result;
+    answer.textContent = randomAnswer(quize.result, quize.mistakenResult);
 
-    // Quize.prototype.progressBar = document.getElementsByClassName('progress-bar')[0];
-		console.log(a,b, result);
-	}
+    quize.yesButton.addEventListener('click', function () {
+      getTask();
+      checkCorrectAnswer (a, b, quize.result);
+      randomAnswer(quize.result, quize.mistakenResult);
+    });
+
+  }
+
+  function checkCorrectAnswer (a, b, answer) {
+    if (a + b == answer) {
+      console.log('success');
+    } else {
+      console.log('error');
+    }
+    console.log(a,b, answer);
+  }
+
+  function randomAnswer (result, mistakenResult) {
+    var index = Math.random().toFixed(1);
+    if (index < 0.5) {
+        console.log(index,'result', result);
+        return result;
+    } else {
+        console.log(index,'errorResult', mistakenResult);
+        return mistakenResult;
+    }
+  }
 
 
   function getRandomInt(min, max) {
     return Math.round(Math.random() * (max - min)) + min;
   }
 
-  function progress () {
-    var yes = quize.yesButton;
-    yes.addEventListener("click",yesClickEvent);
-    quize.noButton.addEventListener("click", noClickEvent);
-  }
 
-  function noClickEvent() {
-    updateProgressBarCount();
-    checkIsRightAnsver(false);
-  }
-
-  function updateScore() {
-    var score = quize.score;
-    score++;
-    // quize.score.textContent = score;
-  }
-
-  function yesClickEvent() {
-    updateProgressBarCount();
-    checkIsRightAnsver(true);
-  }
-
-  function updateProgressBarCount() {
-    if (count > 0) {
-        count =  count - (count / 2);
-    }
-
-  }
-
-  function checkIsRightAnsver(is_yes) {
-    var a = document.querySelector('.a-value').textContent;
-    var b = document.querySelector('.b-value').textContent;
-    var an = document.querySelector('.answer').textContent;
+    // var yes = quize.yesButton;
+    // yes.addEventListener("click",yesClickEvent);
+    // quize.noButton.addEventListener("click", function () {
+    //   if (a + b == answer) {
+    //     console.log('great job');
+    //   } else {
+    //     console.log('error');
+    //   }
+    // });
 
 
-    a = parseInt(a); 
-    b = parseInt(b);
-    an = parseInt(an);
 
-    if (is_yes) {
-      if (a + b ==  an) {
-        updateScore();
-        return getNextTask();
-      }
-      return triggerEndGame();
-    } else {
-      if (a + b !=  an) {
-        updateScore();
-        return getNextTask();
-      }
-      return triggerEndGame();
-    }
+  // function progress () {
+  //   var yes = quize.yesButton;
+  //   yes.addEventListener("click",yesClickEvent);
+  //   quize.noButton.addEventListener("click", noClickEvent);
+  // }
 
-  }
 
-  function getNextTask() {
-    return getTask();
-  }
+
+  // function checkIsRightAnsver(is_yes) {
+  //   var a = document.querySelector('.a-value').textContent;
+  //   var b = document.querySelector('.b-value').textContent;
+  //   var an = document.querySelector('.answer').textContent;
+
+
+  //   a = parseInt(a); 
+  //   b = parseInt(b);
+  //   an = parseInt(an);
+
+  //   if (is_yes) {
+  //     if (a + b ==  an) {
+  //       return getNextTask();
+  //     }
+  //     return triggerEndGame();
+  //   } else {
+  //     if (a + b !=  an) {
+  //       return getNextTask();
+  //     }
+  //     return triggerEndGame();
+  //   }
+
+  // }
+
 
   function triggerEndGame() {
     quize.playGameBlock.style.display = 'block';
@@ -120,15 +133,16 @@ window.onload = (function() {
     quize.score = 0;
   }
    
-  function updateProgressBar() {
+  function progressBar() {
     var count = 0;
+    var barProgress =  quize.barProgress;
+    barProgress.style.width = 0;
     var interval = setInterval(function() {
       if (count >= 100) {
         clearInterval(interval);
         return triggerEndGame();
       }
       count++;
-      console.log(count);
       barProgress.style.width = count + '%';
     }, 0);
   }
@@ -137,3 +151,23 @@ window.onload = (function() {
   
       
  });
+
+
+
+  // function noClickEvent() {
+  //   updateProgressBarCount();
+  //   // checkIsRightAnsver(false);
+  // }
+
+
+  // function yesClickEvent() {
+  //   updateProgressBarCount();
+  //   // checkIsRightAnsver(true);
+  // }
+
+  // function updateProgressBarCount() {
+  //   if (count > 0) {
+  //       count =  count - (count / 2);
+  //   }
+
+  // }
