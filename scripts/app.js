600ee7c3905c6869782f965df6df390733a292cd
Ekaterina Kashchenko <katikate123@gmail.com>
2016-07-25 23:58:57 +0300

    fixed code

--- scripts/app.js
+++ scripts/app.js
@@ -1,157 +1,167 @@
-window.onload = (function () {
+;(function(window) {
   'use strict';
-  var count = 0;
-  var interval = null;
 
-  function Quize() {
-    this.aValue = null;
-    this.bValue = null;
-    this.result = null;
-    this.operation = null;
-    this.mistakenResult = null;
-    this.answer = null;
-    this.score = null;
-    this.startGameBlock = document.getElementsByClassName('start-game')[0];
-    this.playGameBlock = document.getElementsByClassName('play-game')[0];
-    this.endGameBlock = document.getElementsByClassName('end-game')[0];
-    this.startButton = document.getElementsByClassName('start')[0];
-    this.yesButton = document.getElementsByClassName('yes')[0];
-    this.noButton = document.getElementsByClassName('no')[0];
-    this.scoreElem = document.getElementsByClassName('score')[0];
-    this.bar = document.getElementsByClassName('progress-bar')[0];
-    this.barProgress = document.getElementsByClassName('progress-bar-condition')[0];
-    this.restartButton = document.getElementsByClassName('restart')[0];
-  }
+  var Quize;
+  Quize = (function() {
+    var _changeScreen, _calculator, _elements, _timeout;
 
-  var quize = new Quize();
-
-  function startGame() {
-    showBlock(quize.playGameBlock);
-    quize.score = 0;
-    getTask();
-    
-    if (interval !== null) {
-      clearInterval(interval);
-    }
-
-    count = 0;
-    var barProgress = quize.barProgress;
-    barProgress.style.width = 0;
-
-    interval = setInterval(function () {
-      if (count >= 100) {
-        clearInterval(interval);
-        interval = null;
-        endGame();
+    _elements = {
+      screens: {
+        start:       document.getElementsByClassName('start-game')[0],
+        action:      document.getElementsByClassName('play-game')[0],
+        finish:      document.getElementsByClassName('end-game')[0]
+      },
+      buttons: {
+        start:       document.getElementsByClassName('start')[0],
+        restart:     document.getElementsByClassName('restart')[0],
+        answer: {
+          button:    document.getElementsByClassName('action-buttons'),
+          yes:       document.getElementsByClassName('yes')[0],
+          no:        document.getElementsByClassName('no')[0]
+        }
+      },
+      values: {
+        num1:        document.getElementsByClassName('a-value')[0],
+        num2:        document.getElementsByClassName('b-value')[0],
+        answer:      document.getElementsByClassName('answer')[0],
+        operation:   document.getElementsByClassName('operation')[0]
+      },
+      service: {
+        score:       document.getElementsByClassName('score')[0],
+        progressBar: {
+          wrapper:   document.getElementsByClassName('progress-bar')[0],
+          handler:   document.getElementsByClassName('progress-bar-condition')[0]
+        }
       }
-      count++;
-      barProgress.style.width = count + '%';
-    }, 100);
-  }
+    };
 
-  function getOperation () {
-    var operations = ['+', '-', '/', '*' ];
-    var randomOperation = operations[Math.floor(Math.random() * operations.length)];
-    return randomOperation;
-  }
+    Quize.prototype.operations = ['+', '-', '/', '*'];
 
-  function getTask() {
-    resetProgressBar();
-    var a = getRandomInt(1, 20);
-    quize.aValue = a;
-    var b = getRandomInt(1, 20);
-    quize.bValue = b;
-    quize.operation = getOperation();
-    quize.result = getExample (a,b,quize.operation);
-    console.log(a, quize.operation, b, ' = ', quize.result);
-    quize.mistakenResult = getRandomInt(1, 20);
+    _timeout = new window.Timeout(_elements, 100);
 
-    var aValue = document.querySelector(".a-value");
-    aValue.textContent = a;
-    var bValue = document.querySelector('.b-value');
-    bValue.textContent = b;
-    var operationValue = document.querySelector('.operation');
-    operationValue.textContent = quize.operation;
-    var answer = document.querySelector('.answer');
-    quize.answer = randomAnswer(quize.result, quize.mistakenResult);
-    answer.textContent = quize.answer;
-  }
-
-  quize.startButton.addEventListener("click", function () {
-    startGame();
-  });
-
-  quize.yesButton.addEventListener('click', function () {
-    checkCorrectAnswer(quize.aValue, quize.bValue, quize.answer, true);
-    getTask();
-  });
-
-  quize.noButton.addEventListener('click', function () {
-    checkCorrectAnswer(quize.aValue, quize.bValue, quize.answer, false);
-    getTask();
-  });
-
-  quize.restartButton.addEventListener('click', function () {
-    startGame();
-  });
-
-  function checkCorrectAnswer(a, b, answer, button) {
-    var codition = eval(a + quize.operation + b);
-    if (((codition == answer) && (button === true)) || ((codition != answer) && (button === false))) {
-      console.log(a + ' ' + quize.operation + ' ' + b + ' = ' + answer + ' - success, press ' + button + ' correct answer = ' + quize.result);
-      quize.score++;
-      console.log(quize.score);
-
-    } else {
-      console.log(a + ' ' + quize.operation + ' ' + b + ' = ' + answer + ' - error, press ' + button + ' correct answer = ' + quize.result);
-      endGame();
+    function Quize() {
+      _changeScreen('start');
+      this.init();
     }
- }
 
-  function randomAnswer(result, mistakenResult) {
-    var index = Math.random().toFixed(1);
-    if (index < 0.5) {
+    Quize.prototype.answers = {
+      taskAnswer: null,
+      fakeAnswer: null
+    };
+
+    Quize.prototype.score = 0;
+
+    Quize.prototype.init = function() {
+      this.startGame(false);
+      this.restartGame();
+      this.manager();
+    };
+
+    Quize.prototype.startGame = function(render) {
+      if (typeof render !== 'boolean') { throw new Error('Render must be a boolean.'); }
+
+      var start = function() {
+        _timeout.resetTimeout();
+        _changeScreen('action');
+
+        this.setTask();
+      }.bind(this);
+
+      if (render) {
+        start();
+        return;
+      }
+      _elements.buttons.start.addEventListener('click', start);
+    };
+
+    Quize.prototype.restartGame = function() {
+      _elements.buttons.restart.addEventListener('click', function() {
+        this.startGame(true);
+        this.score = 0;
+      }.bind(this));
+    };
+
+    Quize.prototype.finishGame = function() {
+      _changeScreen('finish');
+
+      _elements.service.score.innerHTML = this.score;
+    };
+
+    Quize.prototype.setTask = function() {
+      var randomizer = this.randomizer();
+      var task = {
+        num1:      randomizer.numberInt(1, 20),
+        num2:      randomizer.numberInt(1, 20),
+        answer:    randomizer.numberInt(1, 20),
+        operation: randomizer.operation()
+      };
+
+      this.answers.taskAnswer = _calculator(task.num1, task.num2, task.operation);
+      this.answers.fakeAnswer = task.answer;
+      var answers = [this.answers.fakeAnswer, this.answers.taskAnswer];
+
+      this.answers.fakeAnswer = answers[randomizer.numberInt(0, 1)];
+
+      _elements.values.num1.innerHTML      = task.num1;
+      _elements.values.num2.innerHTML      = task.num2;
+      _elements.values.operation.innerHTML = task.operation;
+      _elements.values.answer.innerHTML    = this.answers.fakeAnswer;
+    };
+
+    Quize.prototype.manager = function() {
+      document.addEventListener('click', function(event) {
+        if (!event.target.classList.contains('action-buttons')) { return; }
+
+        var rightCondition = this.answers.taskAnswer === this.answers.fakeAnswer;
+        var isYes = event.target.classList.contains('yes');
+
+        console.log(isYes, rightCondition);
+        console.log(this.answers.taskAnswer, this.answers.fakeAnswer);
+
+        if (isYes && !rightCondition || !isYes && rightCondition) { this.finishGame(); return; }
+
+        console.log(this.score);
+
+        this.score++;
+        this.startGame(true);
+      }.bind(this));
+    };
+
+    _calculator = function(num1, num2, operation) {
+      var result = null;
+
+      if (operation === '+') { result = num1 + num2; }
+      if (operation === '-') { result = num1 - num2; }
+      if (operation === '*') { result = num1 * num2; }
+      if (operation === '/') { result = (num1 / num2).toFixed(2); }
+
       return result;
-    } else {
-      return mistakenResult;
-    }
-  }
+    };
 
-  function getExample (a,b,operation) {
-    if (operation == '+') {
-      return a + b;
-    }
-    if (operation == '-') {
-      return a - b;
-    }
-    if (operation == '*') {
-      return a * b;
-    }
-    if (operation == '/') {
-      return (a / b).toFixed(3);
-    }
-  }
-   
+    Quize.prototype.randomizer = function() {
+      return {
+        operation: function() {
+          return this.operations[Math.floor(Math.random() * this.operations.length)];
+        }.bind(this),
+        numberInt: function(min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+      };
+    };
 
-  function getRandomInt(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
-  }
+    _changeScreen = function(screenName, callback) {
+      if (screenName !== 'start' && screenName !== 'action' && screenName !== 'finish') {
+        throw new Error('There is only start, action and finish screens.');
+      }
+      Object.keys(_elements.screens).forEach(function(screen) {
+        _elements.screens[screen].style.display = 'none';
+      });
+      if (typeof callback === 'function') { callback(); }
+      _elements.screens[screenName].style.display = 'block';
+    };
 
-  function endGame() {
-    showBlock(quize.endGameBlock);
-    quize.scoreElem.textContent = quize.score;
-  }
+    return Quize;
+  })();
 
-  function resetProgressBar() {
-    count = 0;
-  }
-
-  function showBlock(block) {
-    quize.startGameBlock.style.display = 'none';
-    quize.playGameBlock.style.display = 'none';
-    quize.endGameBlock.style.display = 'none';
-    block.style.display = 'block';
-  }
-
-  showBlock(quize.startGameBlock);
-});
+  new Quize();
+})(window);
